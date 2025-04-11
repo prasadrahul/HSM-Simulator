b53bfb5aa14e628b1c4a60e51e8375c73e713793
rahul prasad <5107275+prasadrahul@users.noreply.github.com>
2025-04-11 20:58:37 +0530

    Create HsmServerApplication.java

---
+++ com.prasadrahul.hsm.HsmServerApplication.java
@@ -0,0 +1,11 @@
+package com.prasadrahul.hsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsmServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HsmServerApplication.class, args);
+    }
+}
